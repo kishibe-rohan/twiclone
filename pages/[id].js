545959aea38932fe395f8a5a854b9a904d368995
kishibe-rohan <devrohan2000@gmail.com>
2022-03-17 15:54:57 +0530

    added modal component

---
+++ pages/[id].js
@@ -0,0 +1,100 @@
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from '@firebase/firestore'
+import { getProviders, getSession, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+import Modal from '../components/Modal'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import Tweet from '../components/Tweet'
+import { db } from '../firebase'
+import { ArrowLeftIcon } from '@heroicons/react/solid'
+import Comment from '../components/Comment'
+import Head from 'next/head'
+
+function TweetPage({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession()
+  const [isOpen, setIsOpen] = useRecoilState(modalState)
+  const [tweet, setTweet] = useState()
+  const [comments, setComments] = useState([])
+  const router = useRouter()
+  const { id } = router.query
+
+  useEffect(
+    () =>
+      onSnapshot(doc(db, 'tweets', id), (snapshot) => {
+        setTweet(snapshot.data())
+      }),
+    [db]
+  )
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, 'tweets', id, 'comments'),
+          orderBy('timestamp', 'desc')
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  )
+
+  if (!session) return <Login providers={providers} />
+
+  return (
+    <div>
+      <Head>
+        <title>
+          {tweet?.username} on Twitter: "{tweet?.text}"
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
+        <Sidebar />
+        <div className="max-w-2xl flex-grow border-l border-r border-gray-700 sm:ml-[73px] xl:ml-[370px]">
+          <div className="sticky top-0 z-50 flex items-center gap-x-4 border-b border-gray-700 bg-black px-1.5 py-2 text-xl font-semibold text-[#d9d9d9]">
+            <div
+              className="hoverAnimation flex h-9 w-9 items-center justify-center xl:px-0"
+              onClick={() => router.push('/')}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Tweet
+          </div>
+
+          <Tweet id={id} tweet={tweet} tweetPage />
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default TweetPage
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
+    (res) => res.json()
+  )
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
+    (res) => res.json()
+  )
+
+  const providers = await getProviders()
+  const session = await getSession(context)
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
 import Widgets from '../components/Widgets'
 import Login from '../components/Login'
+import Modal from '../components/Modal'
+
 import { getProviders, getSession, useSession } from 'next-auth/react'
+import { modalState } from '../atoms/modalAtom'
+import { useRecoilState } from 'recoil'
 
 export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession()
+  const [isOpen, setIsOpen] = useRecoilState(modalState)
 
   if (!session) return <Login providers={providers} />
 
@@ -17,13 +22,14 @@ export default function Home({ trendingResults, followResults, providers }) {
         <title>Twiclone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="max-auto flex min-h-screen max-w-[1500px] bg-black">
+      <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
         <Sidebar />
         <Feed />
         <Widgets
           trendingResults={trendingResults}
           followResults={followResults}
         />
+        {isOpen && <Modal />}
       </main>
     </div>
   )
